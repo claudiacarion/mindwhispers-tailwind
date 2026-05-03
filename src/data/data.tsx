de2e327910a783fdc860db29d37687a90d1714d5
claudiacarion <claudia.carion@gmail.com>
2026-05-03 17:50:25 +0200

    fix types typo in data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,6 +1,6 @@
-import type { Affirmation } from '../types/Affirmation';
+import type { AffirmationType } from '../types/Affirmation';
 
-export const GetAffirmation = async ():Promise<Affirmation> => {
+export const GetAffirmation = async ():Promise<AffirmationType> => {
   const response = await fetch('/api/?ref=freepublicapis.com')
     .then(res => res.json());
   console.log(response);
