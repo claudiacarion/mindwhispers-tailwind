cf5a78b25561830fa6edebef94456c9fa0b53804
claudiacarion <claudia.carion@gmail.com>
2026-05-04 13:50:04 +0200

    add color override

--- src/components/QuoteDisplay/index.tsx
+++ src/components/QuoteDisplay/index.tsx
@@ -10,12 +10,13 @@ const QuoteDisplay = () => {
   }
 
   return (
-    <div className='p-8 text-center'>
-      <div className="min-w-80 h-80 p-4 my-4 rounded-4xl flex justify-center items-center ">
-        {quote ? <p className="text-dark text-3xl dark:text-highlight">{quote}</p> : <p className="text-dark text-2xl dark:text-highlight">What's on your mind?</p>}
+    <div className='p-4 text-center'>
+      <div className="min-w-80 h-80 md:h-50 p-4 my-4 rounded-4xl flex justify-center items-center">
+        {quote ? <p className="text-amber-950 text-3xl dark:text-highlight">{quote}</p> : <p className="text-dark text-xl dark:text-highlight">What do you need to hear?</p>}
       </div>
       <button onClick={handleClick} className="bg-primary opacity-90 p-4 rounded-4xl shadow-lg shadow-primary/50 cursor-pointer">Inspire Me</button>
-    </div>  )
+    </div>
+  )
 }
 
 export default QuoteDisplay
