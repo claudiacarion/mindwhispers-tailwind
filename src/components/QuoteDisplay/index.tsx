4ea51ec3b652cc3d1ec70a5ddebf38e3ce4fe4d9
claudiacarion <claudia.carion@gmail.com>
2026-05-04 14:08:16 +0200

    change color

--- src/components/QuoteDisplay/index.tsx
+++ src/components/QuoteDisplay/index.tsx
@@ -12,7 +12,7 @@ const QuoteDisplay = () => {
   return (
     <div className='p-4 text-center'>
       <div className="min-w-80 h-80 md:h-50 p-4 my-4 rounded-4xl flex justify-center items-center">
-        {quote ? <p className="text-amber-950 text-3xl dark:text-highlight">{quote}</p> : <p className="text-dark text-xl dark:text-highlight">What do you need to hear?</p>}
+        {quote ? <p className="text-amber-950 text-3xl dark:text-highlight">{quote}</p> : <p className="text-amber-950 text-xl dark:text-highlight">What do you need to hear?</p>}
       </div>
       <button onClick={handleClick} className="bg-primary opacity-90 p-4 rounded-4xl shadow-lg shadow-primary/50 cursor-pointer">Inspire Me</button>
     </div>
